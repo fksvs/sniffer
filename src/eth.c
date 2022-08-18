2f6dc4a579404d34d80bd89e6d005c235353baf6
Fatih Koçer <fatihkocerc@gmail.com>
2022-08-18 20:20:19 +0300

    revert to use ethhdr

--- src/eth.c
+++ src/eth.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include <netinet/if_ether.h>
 #include "eth.h"
-#include "type.h"
 
 void ethernet_header(char *buffer)
 {
-	struct eth_hdr *eth = (struct eth_hdr *)buffer;
+	struct ethhdr *eth = (struct ethhdr *)buffer;
 
 	printf("ethernet | %.2x-%.2x-%.2x-%.2x-%.2x-%.2x --> \
 %.2x-%.2x-%.2x-%.2x-%.2x-%.2x- %u\n",
-	       eth->src[0], eth->src[1], eth->src[2],
-	       eth->src[3], eth->src[4], eth->src[5],
-	       eth->dst[0], eth->dst[1], eth->dst[2], eth->dst[3],
-	       eth->dst[4], eth->dst[5], eth->protocol);
+	       eth->h_source[0], eth->h_source[1], eth->h_source[2],
+	       eth->h_source[3], eth->h_source[4], eth->h_source[5],
+	       eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3],
+	       eth->h_dest[4], eth->h_dest[5], eth->h_proto);
 }
