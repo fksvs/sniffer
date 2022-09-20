e4e08f56b33a2c87e8f887f2d8cd624e916f33f9
Fatih Koçer <fatihkocerc@gmail.com>
2022-09-20 23:59:10 +0300

    change method for checking icmp type and tcp flag

--- src/icmp.c
+++ src/icmp.c
@@ -7,53 +7,25 @@
 void icmp_packet(char *buffer)
 {
 	struct iphdr *iph = (struct iphdr *)(buffer + sizeof(struct ethhdr));
-	struct icmphdr *icmph =
-		(struct icmphdr *)(buffer + sizeof(struct ethhdr) +
-				   (iph->ihl) * 4);
-	char type[32];
-	memset(type, 0, 32);
+	struct icmphdr *icmph = (struct icmphdr *)
+		(buffer + sizeof(struct ethhdr) + (iph->ihl) * 4);
+	char type_str[50];
+	memset(type_str, 0, 50);
 
-	switch ((unsigned int)(icmph->type)) {
-	case 0:
-		strcpy(type, "echo reply");
-		break;
-	case 3:
-		strcpy(type, "dest unreacahble");
-		break;
-	case 4:
-		strcpy(type, "source quench");
-		break;
-	case 5:
-		strcpy(type, "redirect");
-		break;
-	case 8:
-		strcpy(type, "echo request");
-		break;
-	case 11:
-		strcpy(type, "time exceeded");
-		break;
-	case 12:
-		strcpy(type, "parameter unintelligible");
-		break;
-	case 13:
-		strcpy(type, "time-stamp request");
-		break;
-	case 14:
-		strcpy(type, "time-stamp reply");
-		break;
-	case 15:
-		strcpy(type, "information request");
-		break;
-	case 16:
-		strcpy(type, "information reply");
-		break;
-	case 17:
-		strcpy(type, "address mask request");
-		break;
-	case 18:
-		strcpy(type, "address mask reply");
-		break;
-	}
+        if (icmph->type == 0) strcpy(type_str, "echo reply");
+        if (icmph->type == 3) strcpy(type_str, "destination unreachable");
+        if (icmph->type == 4) strcpy(type_str, "source quench");
+        if (icmph->type == 5) strcpy(type_str, "redirect");
+        if (icmph->type == 8) strcpy(type_str, "echo request");
+        if (icmph->type == 11) strcpy(type_str, "time exceeded");
+        if (icmph->type == 12) strcpy(type_str, "parameter unintelligible");
+        if (icmph->type == 13) strcpy(type_str, "time-stamp request");
+        if (icmph->type == 14) strcpy(type_str, "time-stamp reply");
+        if (icmph->type == 15) strcpy(type_str, "information request");
+        if (icmph->type == 16) strcpy(type_str, "information reply");
+        if (icmph->type == 17) strcpy(type_str, "address mask request");
+        if (icmph->type == 18) strcpy(type_str, "address mask reply");
+
 	printf("icmp     | type:%d-%s code:%d\n", (unsigned int)(icmph->type),
-	       type, (unsigned int)(icmph->code));
+	       type_str, (unsigned int)(icmph->code));
 }
--- src/tcp.c
+++ src/tcp.c
@@ -8,26 +8,20 @@
 void tcp_header(char *buffer)
 {
 	struct iphdr *iph = (struct iphdr *)(buffer + sizeof(struct ethhdr));
-	struct tcphdr *tcph = (struct tcphdr *)(buffer + sizeof(struct ethhdr) +
-						(iph->ihl) * 4);
-	char tcp_flag[50];
-	memset(tcp_flag, 0, sizeof(tcp_flag));
+	struct tcphdr *tcph = (struct tcphdr *)
+		(buffer + sizeof(struct ethhdr) + (iph->ihl) * 4);
+	char flag_str[32];
+	memset(flag_str, 0, 32);
 
-	if ((unsigned int)(tcph->urg) == 1)
-		strcat(tcp_flag, "urg,");
-	if ((unsigned int)(tcph->ack) == 1)
-		strcat(tcp_flag, "ack,");
-	if ((unsigned int)(tcph->rst) == 1)
-		strcat(tcp_flag, "rst,");
-	if ((unsigned int)(tcph->psh) == 1)
-		strcat(tcp_flag, "psh,");
-	if ((unsigned int)(tcph->syn) == 1)
-		strcat(tcp_flag, "syn,");
-	if ((unsigned int)(tcph->fin) == 1)
-		strcat(tcp_flag, "fin,");
+        if (tcph->fin) strcat(flag_str, "fin,");
+        if (tcph->syn) strcat(flag_str, "syn,");
+        if (tcph->rst) strcat(flag_str, "rst,");
+        if (tcph->psh) strcat(flag_str, "psh,");
+        if (tcph->ack) strcat(flag_str, "ack,");
+        if (tcph->urg) strcat(flag_str, "urg,");
 
 	printf("tcp      | %d --> %d seq:%u ack:%u flag:%s win:%d len:%d\n",
 	       ntohs(tcph->source), ntohs(tcph->dest), ntohl(tcph->seq),
-	       ntohl(tcph->ack_seq), tcp_flag, ntohs(tcph->window),
+	       ntohl(tcph->ack_seq), flag_str, ntohs(tcph->window),
 	       (unsigned int)(tcph->doff) * 4);
 }
